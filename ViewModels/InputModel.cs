c74db71fdf6414f8b88a4a55b4e943e4e70d6dc6
shehzaad.ahmad <https://gitlab.pitb.gov.pk/shehzaad.ahmad>
2022-10-21 00:50:46 +0500

    Add project files.

---
+++ ViewModels/InputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Biometric.ViewModels
+{
+    public class InputModel
+    {
+        public string RegNo { get; set; }
+        public string ChasisNo { get; set; } 
+        public string EngineNo { get; set; }
+    }
+}
